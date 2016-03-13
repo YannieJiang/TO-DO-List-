c0bfa39395c367f42b1d3bd1cf26ee3757932f3d
Yuki <yuki@YukitekiMacBook-Air.local>
2016-03-13 15:37:36 -0700

    Formatting and remove UNDO button

--- todo.js
+++ todo.js
@@ -1,81 +1,30 @@
 /**
- * Created by yuki on 3/11/16.
+ * Created by Yannie
  */
 
 var myList = angular.module("myList",[])
                     .controller('mainCtrl',function($scope){
 
         $scope. itemList=[
-            {'title':'BOO BOO You Put it Down You have to do it!',
+                {'title':'HELLO BOO BOO! You note it down and you have to do it!',
             'done': false}
         ];
-
         $scope.add = function(){
-        $scope.itemList.push({'title':$scope.items,'done':false});
+            $scope.itemList.push({'title':$scope.items,'done':false});
             $scope.items= "";
         };
-
+        //clear completed
         $scope.clearCompleted = function(){
-            $scope.itemList = $scope.itemList.filter(
-            function(val){
-        return !val.done  //return item.done that is not check, false
-    })
-
-        };
-
+            $scope.itemList = $scope.itemList.filter(function(val){
+                            return !val.done  //return item.done that is not check, false
+            })
+        }
         //click to edit item
-
-
         $scope.toEdit = function(){
             if (event.target.contentEditable =="false"){
                 event.target.contentEditable = "true"
                  }
-                 console.log(event.target.contentEditable)
-                 $scope.hideUdo =true;
+        }
 
-                }
-
-                //unto item
-                $scope.undo =function(){
-                  $scope.itemList = $scope.itemList.filter(function(item){
-                    return !item.done + item.done
-                    console.log(item.done)
-                  })
-                }
     });
 
-//     //second To-do List
-// .controller('listCtrl',function($scope){
-//
-//         // declare list item :title, status
-//         $scope.listItem =[
-//             {'title': 'Enter Your Items',
-//             'done':false
-//             }
-//         ];
-//         //add input item to the list
-//         $scope.add = function(){
-//             $scope.listItem.push({'title':$scope.todoList, 'done':false});
-//             $scope.todoList ="";
-//         };
-//
-//
-// // filter out the listitem array, apply filter () to set a call back that returns item.don that's uncheck -has false value
-//         $scope.clearList=function() {
-//             $scope.listItem = $scope.listItem.filter(function (list) {
-//                 return !list.done
-//             })
-//         };
-//
-//             $scope.clickToEdit = function(){
-//
-//                 if(event.target.contentEditable =="false"){
-//                     event.target.contentEditable = "true"
-//                 }
-//                     console.log(event.target.contentEditable)
-//             }
-//
-//
-//
-//
-// });
